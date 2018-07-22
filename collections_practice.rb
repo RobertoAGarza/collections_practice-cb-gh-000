a3ea1267700f8a7eb7f622270b7ab8d7e7bdfe3d
Roberto Garza <robertg4567@hotmail.com>
2018-07-22 23:10:52 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,27 @@
 
+def sort_array_asc(array)
+  array.sort()
+end
+
+def sort_array_desc(array)
+  array.sort!() {|a,b| b <=> a}
+end
+
+def sort_array_char_count(array)
+  array.sort() {|a,b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end
+  }
+end
+
+def swap_elements(array)
+  temp = array[1]
+  array[1] = array[2]
+  array[2] = temp
+  array
+end
